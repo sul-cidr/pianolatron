b510f72175a88f0168d69322075c7722b7002c5f
Simon Wiles <simonjwiles@gmail.com>
2020-10-27 22:30:53 -0700

    Add initial ScoreDetails component

---
+++ src/components/ScoreDetails.svelte
@@ -0,0 +1,32 @@
+<style>
+  ul {
+    background: beige;
+    border-bottom: 1px solid black;
+    columns: 2;
+    display: block;
+    list-style: none;
+    margin: 0;
+    padding: 0.5em 2em;
+  }
+
+  strong {
+    display: inline-block;
+    width: 6em;
+  }
+</style>
+
+<script>
+  export let rollMetadata;
+</script>
+
+<ul>
+  <li><strong>Title:</strong> {rollMetadata.TITLE}</li>
+  <li><strong>Performer:</strong> {rollMetadata.PERFORMER}</li>
+  <li><strong>Composer:</strong> {rollMetadata.COMPOSER}</li>
+  <li><strong>Label:</strong> {rollMetadata.LABEL}</li>
+  <li>
+    <strong>PURL:</strong>
+    <a href="{rollMetadata.PURL}">{rollMetadata.PURL}</a>
+  </li>
+  <li><strong>Call No:</strong> {rollMetadata.CALLNUM}</li>
+</ul>
