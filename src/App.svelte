9a05edc254504c7e65a05b6e7b1b3acc533c22fe
Simon Wiles <simonjwiles@gmail.com>
2020-10-27 22:31:24 -0700

    Update App component to show ScoreDetails

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,16 @@
 <script>
+  import {
+    midiSamplePlayer,
+    rollMetadata,
+  } from "./components/SamplePlayer.svelte";
+  import ScoreDetails from "./components/ScoreDetails.svelte";
+
+  import mididata from "./assets/mididata.json";
+
   const title = "Pianolatron!";
+
+  midiSamplePlayer.loadDataUri(mididata.mozart_rondo_alla_turca);
 </script>
 
 <h1>{title}</h1>
+<ScoreDetails {rollMetadata} />
