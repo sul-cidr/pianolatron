e8e53ff7a6cfb6210fcd15c951e50d75ab6ccfae
Simon Wiles <simonjwiles@gmail.com>
2020-10-28 15:36:13 -0700

    Add (really) minimal ScoreControls component

---
+++ src/components/ScoreControls.svelte
@@ -0,0 +1,9 @@
+<script>
+  export let playPauseMidiFile;
+  export let stopMidiFile;
+</script>
+
+<div id="score-controls">
+  <button type="button" on:click={playPauseMidiFile}>Play/Pause</button>
+  <button type="button" on:click={stopMidiFile}>Stop</button>
+</div>
