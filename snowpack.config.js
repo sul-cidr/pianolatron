b392640b151d718564ba55a62160949d3727cc09
Simon Wiles <simonjwiles@gmail.com>
2021-01-13 22:28:00 -0800

    Add initial snowpack.config.js

---
+++ snowpack.config.js
@@ -0,0 +1,14 @@
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  mount: {
+    src: "/",
+  },
+  plugins: ["@snowpack/plugin-svelte"],
+  routes: [],
+  optimize: {},
+  packageOptions: {},
+  devOptions: {
+    open: "none",
+  },
+  buildOptions: {},
+};
