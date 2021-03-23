1a3d24dc160d1dba8040dc0ff681779dddc637a3
Simon Wiles <simonjwiles@gmail.com>
2021-03-22 23:34:36 -0700

    Better handling of noops.

--- bin/gh-deploy.sh
+++ bin/gh-deploy.sh
@@ -34,8 +34,21 @@ touch "$BUILD_FOLDER/.nojekyll";
 git --work-tree "$BUILD_FOLDER" reset --mixed --quiet;
 git --work-tree "$BUILD_FOLDER" add --all;
 
-git --work-tree "$BUILD_FOLDER" commit -m "$COMMIT_MESSAGE";
+set +o errexit
+diff=$(git --work-tree "$BUILD_FOLDER" diff --exit-code --quiet HEAD --)$?
+set -o errexit
+case $diff in
+  0) echo "$BUILD_FOLDER unchanged -- nothing to commit.";
+     exit 0;
+     ;;
+  1) ;;
+  *)
+    echo "git diff exited with code $diff -- aborting.  Use 'git checkout --force $MAIN_BRANCH' to return to $MAIN_BRANCH." >&2
+    exit "$diff"
+    ;;
+esac
 
+git --work-tree "$BUILD_FOLDER" commit -m "$COMMIT_MESSAGE";
 git push origin "$DEPLOY_BRANCH";
 
 git checkout --force "$MAIN_BRANCH";
