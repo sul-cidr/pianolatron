5739aef158d49a2fab69441f4b593ef9313a02f9
Simon Wiles <simonjwiles@gmail.com>
2021-03-22 23:43:28 -0700

    Reverse deploy logic if changes exist to be comitted

--- bin/gh-deploy.sh
+++ bin/gh-deploy.sh
@@ -35,17 +35,14 @@ set +o errexit
 diff=$(git --work-tree "$BUILD_FOLDER" diff --exit-code --quiet HEAD --)$?
 set -o errexit
 case $diff in
-  0) echo "$BUILD_FOLDER unchanged -- nothing to commit.";
-     exit 0;
+  0) echo "$BUILD_FOLDER unchanged -- nothing to commit.";;
+  1) git --work-tree "$BUILD_FOLDER" commit -m "$COMMIT_MESSAGE";
+     git push origin "$DEPLOY_BRANCH";
      ;;
-  1) ;;
   *)
     echo "git diff exited with code $diff -- aborting.  Use 'git checkout --force $MAIN_BRANCH' to return to $MAIN_BRANCH." >&2
     exit "$diff"
     ;;
 esac
-
-git --work-tree "$BUILD_FOLDER" commit -m "$COMMIT_MESSAGE";
-git push origin "$DEPLOY_BRANCH";
 
 git checkout --force "$MAIN_BRANCH";
