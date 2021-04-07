6aef2c0ebe246df04ad478c192834ae654845dfc
Simon Wiles <simonjwiles@gmail.com>
2021-04-06 22:52:18 -0700

    Add a simple RollSelector component and load MIDI files independently
    The MIDI files *could* be loaded directly from the SUPRA GitHub repo....

--- src/App.svelte
+++ src/App.svelte
@@ -6,20 +6,25 @@
     stopMidiFile,
     skipToPercentage,
   } from "./components/SamplePlayer";
+  import RollSelector from "./components/RollSelector.svelte";
   import RollDetails from "./components/RollDetails.svelte";
   import PlaybackControls from "./components/PlaybackControls.svelte";
 
   const title = "Pianolatron Development";
 
+  let mididataReady;
+  const loadRoll = (druid) => {
+    mididataReady = fetch(`./assets/midi/${druid}.mid`)
+      .then((mididataResponse) => mididataResponse.arrayBuffer())
+      .then((mididataArrayBuffer) => {
+        midiSamplePlayer.stop();
+        midiSamplePlayer.loadArrayBuffer(mididataArrayBuffer);
+      });
+  };
+
+  loadRoll("zb497jz4405");
+
   let appReady = false;
-
-  // Use the url to fetch the file asynchronously
-  const mididataReady = fetch("./assets/mididata.json")
-    .then((mididataResponse) => mididataResponse.json())
-    .then((mididataJson) =>
-      midiSamplePlayer.loadDataUri(mididataJson.mozart_rondo_alla_turca),
-    );
-
   Promise.all([mididataReady, pianoReady]).then(() => {
     appReady = true;
   });
@@ -27,6 +32,7 @@
 
 <h1>{title}</h1>
 {#if appReady}
+  <RollSelector {loadRoll} />
   <RollDetails />
   <PlaybackControls {playPauseMidiFile} {stopMidiFile} {skipToPercentage} />
 {/if}
