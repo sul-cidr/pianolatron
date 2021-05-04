c200360177f277b6cbe9cd780a822fbbd2a9bf75
Simon Wiles <simonjwiles@gmail.com>
2021-05-04 09:25:36 -0700

    wip

--- src/components/RollDetails.svelte
+++ src/components/RollDetails.svelte
@@ -7,9 +7,30 @@
 
   dt {
     font-family: $primary-typeface;
-    font-size: 1.4em;
-    margin-top: 0.5em;
+    font-size: 0.9em;
+    margin-top: 1em;
     margin-bottom: 0.2em;
+    text-transform: uppercase;
+    color: rgba(black, 0.6);
+    display: inline-block;
+
+    &::first-letter {
+      font-size: 1.3em;
+    }
+
+    &:not(.large)::after {
+      content: ":";
+    }
+  }
+
+  dd {
+    font-family: $primary-typeface;
+    display: inline;
+
+    &.large {
+      font-size: 1.4em;
+      display: block;
+    }
   }
 
   dd:not(:has(a)) {
@@ -29,15 +50,15 @@
 
 <dl>
   <dt>Title</dt>
-  <dd>
+  <dd class="large">
     {@html $rollMetadata.TITLE || $rollMetadata.title || unavailable}
   </dd>
   <dt>Performer</dt>
-  <dd>
+  <dd class="large">
     {@html $rollMetadata.PERFORMER || $rollMetadata.performer || unavailable}
   </dd>
   <dt>Composer</dt>
-  <dd>
+  <dd class="large">
     {@html $rollMetadata.COMPOSER || $rollMetadata.composer || unavailable}
   </dd>
   <dt>Label</dt>
