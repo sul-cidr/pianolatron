e09188e1a6ace7c62daf69bddfae7f8ac459bf4c
Simon Wiles <simonjwiles@gmail.com>
2021-05-07 12:44:06 -0700

    Extract the createSetStore factory

--- src/stores.js
+++ src/stores.js
@@ -2,12 +2,25 @@ import { writable } from "svelte/store";
 
 const createStore = (defaultValue) => {
   const { set, subscribe, update } = writable(defaultValue);
-
   return {
     reset: () => set(defaultValue),
     set,
     subscribe,
     update,
+  };
+};
+
+const createSetStore = () => {
+  const { subscribe, update, set } = writable(new Set());
+  return {
+    subscribe,
+    add: (el) => update((wrappedSet) => wrappedSet.add(el)),
+    delete: (el) =>
+      update((wrappedSet) => {
+        wrappedSet.delete(el);
+        return wrappedSet;
+      }),
+    reset: () => set(new Set()),
   };
 };
 
@@ -23,19 +36,7 @@ export const volume = createStore({ master: 1, left: 1, right: 1 });
 export const playbackProgress = createStore(0);
 export const currentTick = createStore(0);
 
-export const activeNotes = (() => {
-  const { subscribe, update, set } = writable(new Set());
-  return {
-    subscribe,
-    add: (el) => update((_activeNotes) => _activeNotes.add(el)),
-    delete: (el) =>
-      update((_activeNotes) => {
-        _activeNotes.delete(el);
-        return _activeNotes;
-      }),
-    reset: () => set(new Set()),
-  };
-})();
+export const activeNotes = createSetStore();
 
 export const userSettings = createStore({
   theme: "cardinal",
