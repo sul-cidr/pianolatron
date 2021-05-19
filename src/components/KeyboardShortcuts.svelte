1b686cde14d63a31785d6e75406a5166e786a473
Simon Wiles <simonjwiles@gmail.com>
2021-05-18 22:17:15 -0700

    Hook up keyboard shortcuts for pedalling
    Do the easy ones first :)

--- src/components/KeyboardShortcuts.svelte
+++ src/components/KeyboardShortcuts.svelte
@@ -1,8 +1,37 @@
 <script>
+  import { pedalling } from "../stores";
+
+  const keyMap = Object.freeze({
+    SOFT: "KeyQ",
+    SUSTAIN: "KeyC",
+  });
 </script>
 
 <svelte:window
-  on:keydown={(e) => {
-    console.log(e);
+  on:keydown={({ code }) => {
+    switch (code) {
+      case keyMap.SOFT:
+        $pedalling.soft = true;
+        break;
+
+      case keyMap.SUSTAIN:
+        $pedalling.sustain = true;
+        break;
+
+      // no default
+    }
+  }}
+  on:keyup={({ code }) => {
+    switch (code) {
+      case keyMap.SOFT:
+        $pedalling.soft = false;
+        break;
+
+      case keyMap.SUSTAIN:
+        $pedalling.sustain = false;
+        break;
+
+      // no default
+    }
   }}
 />
