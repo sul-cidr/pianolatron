13e9658d7cfab73ccb4aee349929ca70af731245
Simon Wiles <simonjwiles@gmail.com>
2021-05-20 16:18:24 -0700

    Rearrange SamplePlayer.svelte
    That's much neater now :)

--- src/components/SamplePlayer.svelte
+++ src/components/SamplePlayer.svelte
@@ -15,9 +15,31 @@
     currentTick,
   } from "../stores";
 
+  let tempoMap;
+
+  const controllerChange = Object.freeze({
+    SUSTAIN_PEDAL: 64,
+    SOFT_PEDAL: 67, // (una corda)
+    PEDAL_ON: 127,
+    PANNING_POSITION: 10,
+  });
+
+  const DEFAULT_NOTE_VELOCITY = 33.0;
+  const SOFT_PEDAL_RATIO = 0.67;
+  const HALF_BOUNDARY = 66; // F# above Middle C; divides the keyboard into two "pans"
+  const ACCENT_BUMP = 1.5;
+
   const midiSamplePlayer = new MidiPlayer.Player();
 
-  let tempoMap;
+  const piano = new Piano({
+    url: "assets/samples/",
+    velocities: 2,
+    release: true,
+    pedal: true,
+    maxPolyphony: 64,
+  }).toDestination();
+
+  const pianoReady = piano.load();
 
   const getTempoAtTick = (tick) => {
     if (!tempoMap) return 60;
@@ -45,72 +67,13 @@
     midiSamplePlayer.setTempo(getTempoAtTick($currentTick) * $tempoCoefficient);
   };
 
-  $: $tempoCoefficient, updatePlayer();
-
-  const decodeHtmlEntities = (string) =>
-    string
-      .replace(/&#(\d+);/g, (match, num) => String.fromCodePoint(num))
-      .replace(/&#x([A-Za-z0-9]+);/g, (match, num) =>
-        String.fromCodePoint(parseInt(num, 16)),
-      );
-
-  midiSamplePlayer.on("fileLoaded", () => {
-    const metadataTrack = midiSamplePlayer.events[0];
-    rollMetadata.set(
-      Object.fromEntries(
-        metadataTrack
-          .filter((event) => event.name === "Text Event")
-          .map((event) =>
-            event.string
-              .match(/^@([^:]*):[\t\s]*(.*)$/)
-              .slice(1, 3)
-              .map((value) => decodeHtmlEntities(value)),
-          ),
-      ),
-    );
-
-    tempoMap = metadataTrack
-      .filter((event) => event.name === "Set Tempo")
-      .reduce((_tempoMap, { tick, data }) => {
-        if (!_tempoMap.map(([, _data]) => _data).includes(data))
-          _tempoMap.push([tick, data]);
-        return _tempoMap;
-      }, []);
-  });
-
-  const controllerChange = Object.freeze({
-    SUSTAIN_PEDAL: 64,
-    SOFT_PEDAL: 67, // (una corda)
-    PEDAL_ON: 127,
-    PANNING_POSITION: 10,
-  });
-
-  const DEFAULT_NOTE_VELOCITY = 33.0;
-  const SOFT_PEDAL_RATIO = 0.67;
-  const HALF_BOUNDARY = 66; // F# above Middle C; divides the keyboard into two "pans"
-  const ACCENT_BUMP = 1.5;
-
-  const panBoundary = HALF_BOUNDARY;
-
-  const piano = new Piano({
-    url: "assets/samples/",
-    velocities: 2,
-    release: true,
-    pedal: true,
-    maxPolyphony: 64,
-  }).toDestination();
-
-  const pianoReady = piano.load();
-
-  $: $sustainOnOff ? piano.pedalDown() : piano.pedalUp();
-
   const startNote = (noteNumber, velocity = DEFAULT_NOTE_VELOCITY) => {
     const modifiedVelocity =
       (velocity / 128) *
       (($softOnOff && SOFT_PEDAL_RATIO) || 1) *
       (($accentOnOff && ACCENT_BUMP) || 1) *
       $volumeCoefficient *
-      (noteNumber < panBoundary
+      (noteNumber < HALF_BOUNDARY
         ? $bassVolumeCoefficient
         : $trebleVolumeCoefficient);
     if (modifiedVelocity) {
@@ -126,8 +89,40 @@
   const stopAllNotes = () => {
     piano.pedalUp();
     if ($sustainOnOff) piano.pedalDown();
-    $activeNotes.forEach((note) => stopNote(note));
+    $activeNotes.forEach(stopNote);
   };
+
+  midiSamplePlayer.on("fileLoaded", () => {
+    const decodeHtmlEntities = (string) =>
+      string
+        .replace(/&#(\d+);/g, (match, num) => String.fromCodePoint(num))
+        .replace(/&#x([A-Za-z0-9]+);/g, (match, num) =>
+          String.fromCodePoint(parseInt(num, 16)),
+        );
+
+    const metadataTrack = midiSamplePlayer.events[0];
+
+    rollMetadata.set(
+      Object.fromEntries(
+        metadataTrack
+          .filter((event) => event.name === "Text Event")
+          .map((event) =>
+            event.string
+              .match(/^@([^:]*):[\t\s]*(.*)$/)
+              .slice(1, 3)
+              .map(decodeHtmlEntities),
+          ),
+      ),
+    );
+
+    tempoMap = metadataTrack
+      .filter((event) => event.name === "Set Tempo")
+      .reduce((_tempoMap, { tick, data }) => {
+        if (!_tempoMap.map(([, _data]) => _data).includes(data))
+          _tempoMap.push([tick, data]);
+        return _tempoMap;
+      }, []);
+  });
 
   midiSamplePlayer.on("playing", ({ tick }) => {
     if (tick <= midiSamplePlayer.totalTicks) currentTick.set(tick);
@@ -138,11 +133,9 @@
     ({ name, value, number, noteNumber, velocity, data }) => {
       if (name === "Note on") {
         if (velocity === 0) {
-          // Note off
           stopNote(noteNumber);
           activeNotes.delete(noteNumber);
         } else {
-          // Note on
           startNote(noteNumber, velocity);
           activeNotes.add(noteNumber);
         }
@@ -163,6 +156,9 @@
       }
     },
   );
+
+  $: $sustainOnOff ? piano.pedalDown() : piano.pedalUp();
+  $: $tempoCoefficient, updatePlayer();
 
   export {
     midiSamplePlayer,
