abb5957bb43c375e7eb509ed01f4659273fb9701
Simon Wiles <simonjwiles@gmail.com>
2021-05-21 20:03:19 -0700

    Move some pure functions into a utilities file

---
+++ src/utils.js
@@ -0,0 +1,6 @@
+export const enforcePrecision = (value, precision) => {
+  const multiplier = 10 ** (precision || 0);
+  return Math.round(value * multiplier) / multiplier;
+};
+
+export const clamp = (value, min, max) => Math.min(Math.max(value, min), max);
