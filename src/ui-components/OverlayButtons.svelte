85a242cf4611adfa64c2eb98386b06ef5be41e03
Simon Wiles <simonjwiles@gmail.com>
2021-05-24 13:40:02 -0700

    Create a simple <OverlayButtons/> wrapper component

---
+++ src/ui-components/OverlayButtons.svelte
@@ -0,0 +1,65 @@
+<style lang="scss">
+  div {
+    background: rgba(0, 0, 0, 0.4);
+    border-radius: 4px;
+    padding: 8px;
+    position: absolute;
+    z-index: z($main-context, overlay-buttons);
+
+    :global(button) {
+      background: none;
+      border: none;
+      color: #ffffff;
+      cursor: pointer;
+      margin: 0;
+      padding: 0.35em 0.8em;
+      transition: all 0.2s;
+
+      &:focus,
+      &:active {
+        outline: 0;
+      }
+
+      &:hover {
+        outline: 1px solid white;
+      }
+
+      &:active {
+        color: grey;
+      }
+
+      &:disabled {
+        color: grey;
+        cursor: not-allowed;
+      }
+    }
+
+    &.top-center {
+      left: 50%;
+      top: 8px;
+      transform: translateX(-50%);
+    }
+
+    &.middle-right {
+      top: 50%;
+      right: 8px;
+      transform: translateY(-50%);
+      display: flex;
+      flex-direction: column;
+
+      :global(button) {
+        padding: 0.8em 0.35em;
+      }
+    }
+  }
+</style>
+
+<script>
+  import { fade } from "svelte/transition";
+
+  export let position;
+</script>
+
+<div class={`overlay-buttons ${position}`} transition:fade>
+  <slot />
+</div>
