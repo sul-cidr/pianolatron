ec4c3d55bcd953419035b1e1f75ddac3843fea5a
Simon Wiles <simonjwiles@gmail.com>
2021-05-25 16:22:04 -0700

    Set sustain pedal only once on Controller Change MIDI event

--- src/components/SamplePlayer.svelte
+++ src/components/SamplePlayer.svelte
@@ -170,13 +170,7 @@
         }
       } else if (name === "Controller Change" && $rollPedalingOnOff) {
         if (number === controllerChange.SUSTAIN_PEDAL) {
-          if (value === controllerChange.PEDAL_ON) {
-            piano.pedalDown();
-            sustainOnOff.set(true);
-          } else {
-            piano.pedalUp();
-            sustainOnOff.set(false);
-          }
+          sustainOnOff.set(value === controllerChange.PEDAL_ON);
         } else if (number === controllerChange.SOFT_PEDAL) {
           softOnOff.set(value === controllerChange.PEDAL_ON);
         }
