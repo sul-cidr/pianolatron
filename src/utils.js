d5b13d403cd0de173399e8f05e234857c0427a11
Simon Wiles <simonjwiles@gmail.com>
2021-07-23 14:11:10 -0700

    Add an `easingInterval(interval, fn)` function
    Calls the wrapped function repeatedly according to a decreasing interval.  The interval decay rate and floor values (but not start point) are hardcoded.

--- src/utils.js
+++ src/utils.js
@@ -4,3 +4,25 @@ export const enforcePrecision = (value, precision) => {
 };
 
 export const clamp = (value, min, max) => Math.min(Math.max(value, min), max);
+
+export const easingInterval = (interval, fn) => {
+  let timeoutId;
+
+  const wrapped = () => {
+    fn();
+    // eslint-disable-next-line no-param-reassign
+    interval = Math.max(Math.floor(interval / 1.2), 10);
+    timeoutId = setTimeout(wrapped, interval);
+  };
+
+  timeoutId = setTimeout(wrapped, interval);
+
+  return {
+    clear: () => {
+      if (timeoutId) {
+        clearTimeout(timeoutId);
+        timeoutId = null;
+      }
+    },
+  };
+};
