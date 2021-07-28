2d2fba2f36b408f0da9353ca0ffc8fc954e6bc48
Simon Wiles <simonjwiles@gmail.com>
2021-07-28 12:12:49 -0700

    Wind off some unneeded borders

--- src/components/RollViewer.svelte
+++ src/components/RollViewer.svelte
@@ -310,12 +310,13 @@
       navigatorLeft: "calc(100% - 40px)",
       navigatorHeight: "100%",
       navigatorWidth: "40px",
-      navigatorDisplayRegionColor: "white",
+      navigatorDisplayRegionColor: "transparent",
     });
 
     ({ viewport } = openSeadragon);
 
     // Override some styles that OSD sets directly on the elements
+    openSeadragon.navigator.element.style.border = "none";
     openSeadragon.navigator.displayRegion.style.display = "block";
     openSeadragon.navigator.displayRegion.style.left = "0";
     openSeadragon.navigator.displayRegion.style.width = "100%";
