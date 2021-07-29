5b10d55351e45a26e3fb8b1aca4b2a94911e530c
Simon Wiles <simonjwiles@gmail.com>
2021-07-29 11:34:12 -0700

    Handle mousewheel zooming manually
    OSD's mousewheel zooming goes crazy if used while the app is playing (I think it's trying to be clever with it's mixed panning or zooming -- in any event, it's really unhelpful in our case).  After trying a number of other solutions, seems that just disabling it and handling the scroll events ourselves produces the best outcome.

--- src/components/RollViewer.svelte
+++ src/components/RollViewer.svelte
@@ -302,7 +302,7 @@
       maxZoomLevel,
       constrainDuringPan: true,
       preserveImageSizeOnResize: true,
-      gestureSettingsMouse: { clickToZoom: false },
+      gestureSettingsMouse: { clickToZoom: false, scrollToZoom: false },
       showNavigator: true,
       navigatorAutoFade: false,
       navigatorPosition: "ABSOLUTE",
@@ -421,7 +421,15 @@
     if (event.ctrlKey) {
       panByIncrement(event.deltaY > 0);
       event.stopPropagation();
+      return;
     }
+
+    viewport.zoomTo(
+      Math.min(
+        viewport.getZoom() * (event.deltaY > 0 ? 0.9 : 1.1),
+        maxZoomLevel,
+      ),
+    );
   }}
   class:active-note-details={$userSettings.activeNoteDetails}
   class:highlight-enabled-holes={$userSettings.highlightEnabledHoles}
