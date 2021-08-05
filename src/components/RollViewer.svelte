b3ea852af3edd578556a6858fc732f715f0d7c82
Simon Wiles <simonjwiles@gmail.com>
2021-08-04 21:54:34 -0700

    Refactor setting of `--trackerbar-height` variable

--- src/components/RollViewer.svelte
+++ src/components/RollViewer.svelte
@@ -117,6 +117,7 @@
   let imageLength;
   let imageWidth;
   let avgHoleWidth;
+  let trackerbarHeight;
 
   const calculateHoleColors = (holeData) => {
     const velocities = holeData.map(({ v }) => v).filter((v) => v);
@@ -328,13 +329,7 @@
     });
     openSeadragon.addHandler("zoom", ({ zoom }) => {
       const imageZoom = viewport.viewportToImageZoom(zoom);
-      const rv = document.getElementById("roll-viewer");
-      if (!rv) return;
-      const trackerbarHeight = Math.max(
-        1,
-        parseInt(avgHoleWidth * imageZoom, 10),
-      );
-      rv.style.setProperty("--trackerbar-height", `${trackerbarHeight}px`);
+      trackerbarHeight = Math.max(1, avgHoleWidth * imageZoom);
     });
     openSeadragon.open(imageUrl);
   });
@@ -387,6 +382,7 @@
   class:show-note-velocities={$userSettings.showNoteVelocities}
   class:use-roll-pedaling={$rollPedalingOnOff}
   class:play-expressions={$playExpressionsOnOff}
+  style="--trackerbar-height: {trackerbarHeight}px"
 >
   {#if !rollImageReady}
     <p transition:fade>Downloading roll image...</p>
