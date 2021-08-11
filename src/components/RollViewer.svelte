972243e8ccd8ad227ff14dc4549b0e76b3aeb378
Simon Wiles <simonjwiles@gmail.com>
2021-08-11 11:12:47 -0700

    Use OSD's `scrollToZoom`
    OSD can exhibit odd behaviour if the nav strip is used to jump around while zoom animations are playing (see https://github.com/sul-cidr/pianolatron/pull/68#issuecomment-896304339).  One way to solve this is to use OSD's own `scrollToZoom`, which doesn't operate when scrolling on the nav strip.
    
    This is effectively a reversion of 5b10d553.

--- src/components/RollViewer.svelte
+++ src/components/RollViewer.svelte
@@ -329,7 +329,7 @@
       maxZoomLevel,
       constrainDuringPan: true,
       preserveImageSizeOnResize: true,
-      gestureSettingsMouse: { clickToZoom: false, scrollToZoom: false },
+      gestureSettingsMouse: { clickToZoom: false, scrollToZoom: true },
       showNavigator: true,
       navigatorAutoFade: false,
       navigatorPosition: "ABSOLUTE",
@@ -450,15 +450,7 @@
     if (event.ctrlKey) {
       panByIncrement(event.deltaY > 0);
       event.stopPropagation();
-      return;
     }
-
-    viewport.zoomTo(
-      Math.min(
-        viewport.getZoom() * (event.deltaY > 0 ? 0.9 : 1.1),
-        maxZoomLevel,
-      ),
-    );
   }}
   class:active-note-details={$userSettings.activeNoteDetails}
   class:highlight-enabled-holes={$userSettings.highlightEnabledHoles}
