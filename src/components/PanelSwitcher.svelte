801e06ad6a6ad13fd41fce3d6e03d1c515cb080a
Peter Broadwell <broadwell@stanford.edu>
2021-08-18 22:33:02 -0700

    Add panel and icon for audio settings

--- src/components/PanelSwitcher.svelte
+++ src/components/PanelSwitcher.svelte
@@ -32,7 +32,7 @@
 <script>
   import Icon from "../ui-components/Icon.svelte";
 
-  const panels = ["controls", "settings"];
+  const panels = ["controls", "settings", "audio"];
   export let selectedPanel;
 </script>
 
@@ -50,6 +50,44 @@
         <Icon name="sliders" height="24" width="24" />
       {:else if panel === "settings"}
         <Icon name="cog" height="24" width="24" />
+      {:else if panel === "audio"}
+        <svg
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          stroke-width="2"
+          stroke="currentColor"
+          fill="none"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          version="1.1"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlns:svg="http://www.w3.org/2000/svg"
+        >
+          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+          <rect x="2" y="7" width="20" height="12" rx="2" />
+          <line
+            x1="7"
+            y1="9"
+            x2="7"
+            y2="13.5"
+            style="stroke-width:2.5;stroke-linecap:square"
+          />
+          <line
+            x1="12"
+            y1="9"
+            x2="12"
+            y2="13.5"
+            style="stroke-width:2.5;stroke-linecap:square"
+          />
+          <line
+            x1="17"
+            y1="9"
+            x2="17"
+            y2="13.5"
+            style="stroke-width:2.5;stroke-linecap:square"
+          />
+        </svg>
       {/if}
     </label>
   {/each}
