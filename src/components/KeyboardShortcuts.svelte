f7fdf72b5586221d03bde5399ed9791d3216dd98
Simon Wiles <simonjwiles@gmail.com>
2021-08-26 16:35:09 -0700

    Update some user facing text
    See conversation / review in #90.

--- src/components/KeyboardShortcuts.svelte
+++ src/components/KeyboardShortcuts.svelte
@@ -76,77 +76,77 @@
       code: "KeyB",
       key: "b",
       description: "Soft Pedal",
-      help: "Hold down to apply the soft pedal",
+      help: "Hold to apply the soft pedal",
     },
     SUSTAIN: {
       code: "Space",
       key: "␣",
       description: "Sustain Pedal",
-      help: "Hold down to apply the sustain pedal",
+      help: "Hold to apply the sustain pedal",
     },
     ACCENT: {
       code: "KeyN",
       key: "n",
       description: "Accent Button",
-      help: "Hold down to apply a bump to the intensity",
+      help: "Notes struck while this is held with be 50% louder",
     },
 
     VOLUME_UP: {
       code: "KeyO",
       key: "o",
       description: "Volume Up",
-      help:
-        "Increase the main volume (use Shift for a larger increment, and Ctrl for finer-grained control)",
+      help: `Increase the main volume
+        Use SHIFT for a larger increment, and CTRL for finer-grained control`,
     },
     VOLUME_DOWN: {
       code: "KeyI",
       key: "i",
       description: "Volume Down",
-      help:
-        "Decrease the main volume (use Shift for a larger increment, and Ctrl for finer-grained control)",
+      help: `Decrease the main volume
+        Use SHIFT for a larger increment, and CTRL for finer-grained control`,
     },
     BASS_VOLUME_UP: {
       code: "Digit4",
       key: "4",
       description: "Bass Volume Up",
-      help:
-        "Increase the bass volume (use Shift for a larger increment, and Ctrl for finer-grained control)",
+      help: `Increase the bass volume
+        Use SHIFT for a larger increment, and CTRL for finer-grained control`,
     },
     BASS_VOLUME_DOWN: {
       code: "KeyE",
       key: "e",
       description: "Bass Volume Down",
-      help:
-        "Decrease the bass volume (use Shift for a larger increment, and Ctrl for finer-grained control)",
+      help: `Decrease the bass volume
+        Use SHIFT for a larger increment, and CTRL for finer-grained control`,
     },
     TREBLE_VOLUME_UP: {
       code: "Digit0",
       key: "0",
       description: "Treble Volume Up",
-      help:
-        "Increase the treble volume (use Shift for a larger increment, and Ctrl for finer-grained control)",
+      help: `Increase the treble volume
+        Use SHIFT for a larger increment, and CTRL for finer-grained control`,
     },
     TREBLE_VOLUME_DOWN: {
       code: "KeyP",
       key: "p",
       description: "Treble Volume Down",
-      help:
-        "Decrease the treble volume (use Shift for a larger increment, and Ctrl for finer-grained control)",
+      help: `Decrease the treble volume
+        Use SHIFT for a larger increment, and CTRL for finer-grained control`,
     },
 
     TEMPO_UP: {
       code: "KeyT",
       key: "t",
       description: "Tempo Up",
-      help:
-        "Increase the tempo (use Shift for a larger increment, and Ctrl for finer-grained control)",
+      help: `Increase the tempo
+        Use SHIFT for a larger increment, and CTRL for finer-grained control`,
     },
     TEMPO_DOWN: {
       code: "KeyR",
       key: "r",
       description: "Tempo Down",
-      help:
-        "Decrease the tempo (use Shift for a larger increment, and Ctrl for finer-grained control)",
+      help: `Decrease the tempo
+        Use SHIFT for a larger increment, and CTRL for finer-grained control`,
     },
 
     PLAY_PAUSE: {
@@ -159,19 +159,19 @@
       code: "Backspace",
       key: "←",
       description: "Rewind Roll",
-      help: "Rewind the roll to the beginning of the holes",
+      help: "Rewind the roll to the beginning",
     },
     FORWARD: {
       code: "Digit8",
       key: "8",
-      description: "Move Roll Forwards",
-      help: "Advance the roll forwards (hold down to accelerate)",
+      description: "Scrub Forwards",
+      help: "Advance the roll (hold to accelerate)",
     },
     BACKWARD: {
       code: "Digit6",
       key: "6",
-      description: "Move Roll Backwards",
-      help: "Move the roll backwards (hold down to accelerate)",
+      description: "Scrub Backwards",
+      help: "Back up (hold to accelerate)",
     },
   };
   export const keyMap = createPersistedStore(
@@ -186,6 +186,7 @@
     "Enter",
     "ShiftLeft",
     "ShiftRight",
+    "CapsLock",
   ];
 
   const showKeybindingsConfig = writable(false);
@@ -287,12 +288,12 @@
         .includes(detail.code) &&
       detail.code !== $keyMap[shortcut].code
     ) {
-      errorMessage = `Cancelled -- the "${detail.key}" key is already assigned!`;
+      errorMessage = `The "${detail.key}" key is already assigned.`;
       return;
     }
 
     if (unusableKeys.includes(detail.code)) {
-      errorMessage = `Cancelled -- the "${detail.key}" key cannot be assigned!`;
+      errorMessage = `The "${detail.key}" key cannot be assigned.`;
       return;
     }
 
@@ -359,7 +360,7 @@
         </svg></button
       >
     </header>
-    <p>Click the edit button to reassign a command's key.</p>
+    <p>Click the edit button to reassign a control button.</p>
     {#if errorMessage}
       <p class="error-message" transition:slide>{errorMessage}</p>
     {/if}
