054c9ae88dd859874358bf715ad5a6004addb0eb
Simon Wiles <simonjwiles@gmail.com>
2021-09-01 10:51:06 -0700

    Handle multiples of 1 unit a little better

--- src/components/RollViewer.svelte
+++ src/components/RollViewer.svelte
@@ -70,7 +70,7 @@
     }
   }
 
-  span {
+  .roll-loading {
     background: rgba(black, 0.4);
     border-radius: 4px;
     color: white;
@@ -80,27 +80,45 @@
     top: 1em;
     z-index: 1;
   }
+
   .scale-bar {
     background-color: rgba(white, 0.5);
-    background-repeat: no-repeat;
-    border-radius: 0;
-    border-top: 0;
-    border: 1px solid black;
     color: black;
     font-size: 12px;
     height: 22px;
-    padding: 0 2px;
     transition: width 0.5s ease;
+    position: absolute;
+    left: 1em;
+    top: 1em;
+    z-index: 1;
+    display: flex;
+    border: 1px solid black;
+    border-top: 0;
+    border-right: 0;
 
-    &.us {
+    &:before {
+      content: attr(data-label);
+      display: block;
+      left: 4px;
+      position: absolute;
+    }
+
+    span {
+      background-repeat: no-repeat;
+      border-radius: 0;
+      flex: 1 0 auto;
+    }
+
+    &.us span {
       background-image: linear-gradient(black, black),
         linear-gradient(black, black), linear-gradient(black, black),
         linear-gradient(black, black), linear-gradient(black, black),
-        linear-gradient(black, black), linear-gradient(black, black);
+        linear-gradient(black, black), linear-gradient(black, black),
+        linear-gradient(black, black);
       background-position: 12.5% bottom, 25% bottom, 37.5% bottom, 50% bottom,
-        62.5% bottom, 75% bottom, 87.5% bottom;
+        62.5% bottom, 75% bottom, 87.5% bottom, 100% bottom;
       background-size: 1px 6px, 1px 9px, 1px 6px, 1px 12px, 1px 6px, 1px 9px,
-        1px 6px;
+        1px 6px, 1px 22px;
     }
   }
 </style>
@@ -507,7 +525,11 @@
       const ppi = viewport.viewportToImageZoom(viewport.getZoom(true)) * 300;
       let inches = 1;
       while (ppi * inches < minScaleBarWidth) inches += 1;
-      scaleBarWidth = { px: ppi * inches, label: `${inches}in` };
+      scaleBarWidth = {
+        px: ppi * inches,
+        label: `${inches}in`,
+        multiples: inches,
+      };
     });
 
     openSeadragon.open(imageUrl);
@@ -555,11 +577,16 @@
   {#if !rollImageReady}
     <span class="roll-loading" transition:fade>Downloading roll image...</span>
   {:else}
-    <span
+    <div
       class="scale-bar us"
       transition:fade
-      style={`width: ${scaleBarWidth.px}px`}>{scaleBarWidth.label}</span
+      style={`width: ${scaleBarWidth.px}px`}
+      data-label={scaleBarWidth.label}
     >
+      {#each Array(scaleBarWidth.multiples) as _}
+        <span />
+      {/each}
+    </div>
   {/if}
   {#if showControls}
     <RollViewerControls
