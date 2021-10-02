cae7e6c134751e64c006481b8fc6708779c27c28
Simon Wiles <simonjwiles@gmail.com>
2021-10-01 23:15:14 -0700

    Split keyboard shortcut editor layout into "tabs"
    I'm not sure to what extent this is going to fly, tbh.

--- src/components/KeyboardShortcutEditor.svelte
+++ src/components/KeyboardShortcutEditor.svelte
@@ -1,20 +1,43 @@
 <style lang="scss">
-  div {
+  .shortcut-editor {
     background-color: var(--background-color);
     border-radius: 4px;
     box-shadow: 0 3px 6px rgb(0, 0, 0, 0.3);
+    display: flex;
+    flex-direction: column;
     left: 50%;
     max-height: 80%;
-    max-width: 100%;
-    min-width: 400px;
+    max-width: 100vw;
+    min-width: min(400px, 100vw);
     padding: 1em;
     position: absolute;
     top: 42%;
     transform: translate(-50%, -50%);
     width: 400px;
     z-index: z($main-context, notifications);
-    display: flex;
-    flex-direction: column;
+
+    :global(.panel-switcher) {
+      border-bottom: 1px solid rgba(0, 0, 0, 0.4);
+    }
+
+    :global(.panel-switcher label) {
+      background-color: var(--primary-accent);
+      border-bottom: 0;
+      border-radius: 0.25em 0.25em 0 0;
+      border-width: 1px;
+      color: white;
+      opacity: 0.8;
+      padding-bottom: 4px;
+    }
+
+    :global(.panel-switcher label:last-child) {
+      margin-right: 4px;
+    }
+    :global(.panel-switcher input:checked + label) {
+      background-color: var(--background-color);
+      color: black;
+      transform: translateY(2px);
+    }
   }
 
   header {
@@ -39,11 +62,14 @@
 
   dl {
     display: grid;
-    grid-template-columns: auto auto;
-    justify-content: space-between;
     gap: 0 0.25em;
-    overflow-y: auto;
+    grid-auto-rows: min-content;
+    grid-template-columns: auto auto;
+    height: 400px;
+    justify-content: space-between;
     overflow-x: hidden;
+    overflow-y: auto;
+    padding-top: 4px;
   }
 
   p.reset {
@@ -80,6 +106,7 @@
 
 <script>
   import { fade, slide } from "svelte/transition";
+  import PanelSwitcher from "./PanelSwitcher.svelte";
   import KeyboardShortcutEditorRow from "./KeyboardShortcutEditorRow.svelte";
   import Icon from "../ui-components/Icon.svelte";
   import {
@@ -91,6 +118,47 @@
   import { keyMap } from "./KeyboardShortcuts.svelte";
 
   let errorMessage;
+  let selectedPanel = "playback";
+
+  const panels = {
+    playback: {
+      text: "Playback Controls",
+      shortcuts: [
+        "SOFT",
+        "SUSTAIN",
+        "ACCENT",
+        "PLAY_PAUSE",
+        "REWIND",
+        "FORWARD",
+        "BACKWARD",
+      ],
+    },
+    volumeAndTempo: {
+      text: "Volume & Tempo",
+      shortcuts: [
+        "VOLUME_UP",
+        "VOLUME_DOWN",
+        "BASS_VOLUME_UP",
+        "BASS_VOLUME_DOWN",
+        "TREBLE_VOLUME_UP",
+        "TREBLE_VOLUME_DOWN",
+        "TEMPO_UP",
+        "TEMPO_DOWN",
+        "LEFT_HAND_AUGMENT",
+        "RIGHT_HAND_AUGMENT",
+      ],
+    },
+  };
+
+  panels.others = {
+    text: "Others",
+    shortcuts: Object.keys($keyMap).filter(
+      (shortcut) =>
+        !Array.prototype
+          .concat(...Object.values(panels).map((p) => p.shortcuts))
+          .includes(shortcut),
+    ),
+  };
 
   const updateKeyBinding = (shortcut, detail) => {
     errorMessage = undefined;
@@ -123,7 +191,7 @@
 </script>
 
 {#if $showKeybindingsConfig}
-  <div transition:fade>
+  <div class="shortcut-editor" transition:fade>
     <header>
       Keyboard Controls
       <button on:click={toggleKeybindingsConfig}>
@@ -134,8 +202,9 @@
     {#if errorMessage}
       <p class="error-message" transition:slide>{errorMessage}</p>
     {/if}
+    <PanelSwitcher bind:selectedPanel {panels} class="panel-switcher" />
     <dl>
-      {#each Object.keys($keyMap) as shortcut}
+      {#each panels[selectedPanel].shortcuts as shortcut}
         <KeyboardShortcutEditorRow
           shortcut={$keyMap[shortcut]}
           meta={keyMapMeta[shortcut]}
