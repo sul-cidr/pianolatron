58d30f56a86a6837c56ef87e5223b3c1e3fb2d6e
Simon Wiles <simonjwiles@gmail.com>
2021-10-11 13:48:37 -0700

    Cleanup using globals.scss to set CSS variables
    `globals.scss` is included in (more-or-less) every SCSS declaration, so should only contain SASS variables that compile away.

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,7 @@ const sveltePreprocess = require("svelte-preprocess");
 module.exports = {
   preprocess: sveltePreprocess({
     scss: {
-      prependData: `@use "sass:math";@import 'src/styles/globals.scss';`,
+      prependData: `@use "sass:math";@import 'src/styles/sass-globals.scss';`,
     },
   }),
 };
