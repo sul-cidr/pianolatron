sul-cidr/pianolatron

commit c36ea6c730d9ff2a9b4e30cefa8d28cad7162d91

Move mousewheel handling back to `<RangeSlider/>`

src/ui-components/RangeSlider.svelte
@@ -172,6 +172,28 @@
   export let step;
   export let name;
   export let value;
+  export let mousewheel = true;
+
+  const clamp = (_value) => Math.min(Math.max(_value, min), max);
+
+  const handleWheel = ({ deltaY }) => {
+    if (!mousewheel) return;
+    const precision = (step.toString().split(".")[1] || "").length;
+    if (deltaY > 0) {
+      value = clamp((Number(value) + Number(step)).toFixed(precision));
+    } else {
+      value = clamp((Number(value) - Number(step)).toFixed(precision));
+    }
+  };
 </script>
 
-<input type="range" bind:value on:input on:change {min} {max} {step} {name} />
+<input
+  type="range"
+  bind:value
+  on:input
+  on:change
+  on:mousewheel={handleWheel}
+  {max}
+  {step}
+  {name}
+/>

src/ui-components/SliderControl.svelte
@@ -25,22 +25,19 @@
   export let step;
   export let name;
   export let mousewheel = true;
-
-  const clamp = (_value) => Math.min(Math.max(_value, min), max);
-
-  const handleWheel = ({ deltaY }) => {
-    if (!mousewheel) return;
-    const precision = (step.toString().split(".")[1] || "").length;
-    if (deltaY > 0) {
-      value = clamp((Number(value) + Number(step)).toFixed(precision));
-    } else {
-      value = clamp((Number(value) - Number(step)).toFixed(precision));
-    }
-  };
 </script>
 
-<div on:mousewheel={handleWheel}>
+<div>
   <span><slot name="label">{name}</slot></span>
   <span><slot name="value">{value}</slot></span>
-  <RangeSlider bind:value on:input on:change {min} {max} {step} {name} />
+  <RangeSlider
+    bind:value
+    {min}
+    {max}
+    {step}
+    {name}
+    {mousewheel}
+    on:input
+    on:change
+  />
 </div>

commit 797e2aec00cc3cef9f1881da70d543789c912b23

Fix error introduced in c36ea6c

src/ui-components/RangeSlider.svelte
@@ -193,6 +193,7 @@
   on:input
   on:change
   on:mousewheel={handleWheel}
+  {min}
   {max}
   {step}
   {name}