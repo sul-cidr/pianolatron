a35c450bfdaf0e539abb4cfec434bc941751edf2
Simon Wiles <simonjwiles@gmail.com>
2022-01-20 08:51:52 -0800

    Default WebMIDI functionality to off

--- src/stores.js
+++ src/stores.js
@@ -88,7 +88,7 @@ export const userSettings = createPersistedStore("userSettings", {
   showKeyboard: true,
   overlayKeyboard: false,
   welcomeScreenInhibited: false,
-  useWebMidi: !!navigator.requestMIDIAccess,
+  useWebMidi: false,
 });
 
 // Browser State
