2ef5260ad9d61dcb5aa45e91916c07bf2b9fbbdf
Simon Wiles <simonjwiles@gmail.com>
2022-09-08 12:38:22 -0700

    Add welte-licensee support
    Note: there are issues here to be resolved.

--- src/expression-boxes/index.js
+++ src/expression-boxes/index.js
@@ -1,9 +1,11 @@
 import ExpressiveMidiExpressionizer from "./expressive-midi";
 import WelteRedExpressionizer from "./welte-red";
 import WelteGreenExpressionizer from "./welte-green";
+import WelteLicenseeExpressionizer from "./welte-licensee";
 
 export default {
   expressiveMidi: ExpressiveMidiExpressionizer,
   "welte-red": WelteRedExpressionizer,
   "welte-green": WelteGreenExpressionizer,
+  "welte-licensee": WelteLicenseeExpressionizer,
 };
