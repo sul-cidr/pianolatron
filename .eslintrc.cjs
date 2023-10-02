88d84cb4041cd808237ec440657669a4f1fdb72a
Simon Wiles <simonjwiles@gmail.com>
2023-10-02 15:39:48 -0700

    Replace deprecated `eslint-plugin-svelte3` with `eslint-plugin-svelte`
    This seems to break hard with the `airbnb-base` settings we were using, so I'm defaulting to the `eslint-plugin-svelte` recommended settings and will build back (where necessary) from there.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,34 +1,14 @@
 module.exports = {
   root: true,
-  plugins: ["svelte3"],
-  extends: ["airbnb-base", "prettier"],
+  extends: ["eslint:recommended", "plugin:svelte/recommended", "prettier"],
   parserOptions: {
-    ecmaVersion: 2020,
     sourceType: "module",
+    ecmaVersion: 2020,
+    extraFileExtensions: [".svelte"],
   },
   env: {
-    es6: true,
     browser: true,
-  },
-  overrides: [
-    {
-      files: ["**/*.svelte"],
-      processor: "svelte3/svelte3",
-    },
-  ],
-  rules: {
-    "no-cond-assign": ["error", "except-parens"],
-    "no-param-reassign": ["error", { props: false }],
-    "no-return-assign": ["error", "except-parens"],
-    "no-undef-init": "off",
-    "no-underscore-dangle": "off",
-    // see https://github.com/sveltejs/eslint-plugin-svelte3/blob/master/OTHER_PLUGINS.md#eslint-plugin-import
-    "import/first": "off",
-    "import/no-mutable-exports": "off",
-    "import/prefer-default-export": "off",
-    "import/no-duplicates": "off",
-  },
-  settings: {
-    "svelte3/ignore-styles": (attribs) => "lang" in attribs,
+    es2017: true,
+    node: true,
   },
 };
