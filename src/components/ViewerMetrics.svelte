ae7b8c373b2b8e9368ca563d0c8bf720586db2c5
Peter Broadwell <broadwell@stanford.edu>
2024-03-20 00:18:58 -0700

    Default values for feet/min and time index (incidental fix)

--- src/components/ViewerMetrics.svelte
+++ src/components/ViewerMetrics.svelte
@@ -19,18 +19,23 @@
 </style>
 
 <script>
-  import { currentTick, ticksPerSecond } from "../stores";
+  import { currentTick, ticksPerSecond, rollMetadata } from "../stores";
 </script>
 
 <div id="viewer-metrics">
   <div class="metric">
     <span>Feet per Minute:</span><span
-      >{(($ticksPerSecond / 300 / 12) * 60).toFixed(2)}</span
+      >{($ticksPerSecond
+        ? ($ticksPerSecond / 300 / 12) * 60
+        : ($rollMetadata.TICKS_PER_QUARTER * 60) / (300 * 12.0)
+      ).toFixed(2)}</span
     >
   </div>
   <div class="metric">
-    <span>Time index:</span><span
-      >{($currentTick / $ticksPerSecond).toFixed(1)}s</span
+    <span>Time Index:</span><span
+      >{($ticksPerSecond ? $currentTick / $ticksPerSecond : 0).toFixed(
+        1,
+      )}s</span
     >
   </div>
 </div>
