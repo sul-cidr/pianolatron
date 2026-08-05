3aafb7a46f89525d4e12294ee64a1a9e9a852ae1
Peter Broadwell <broadwell@stanford.edu>
2026-08-05 13:09:05 +0900

    Hotfix: Stop all MIDI notes on pause

--- src/components/WebMidi.svelte
+++ src/components/WebMidi.svelte
@@ -255,7 +255,7 @@
       midiBytes[entity] || entity,
       clamp(parseInt(value * 127, 10), 0, 127),
     ];
-    if ($isPlaying) $midiOutputs.forEach((output) => output.send(msg));
+    $midiOutputs.forEach((output) => output.send(msg));
     if ($recordingOnOff) {
       const now = Date.now();
       if (msgType === "NOTE_ON" && !(entity in heldDown)) {
